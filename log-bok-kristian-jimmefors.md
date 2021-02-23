2ab8e722fb86cdcd420723d466010363677a4d4c
Kristian Jimmefors <kristian.jimmefors@gmail.com>
2021-02-23 14:37:52 +0100

    Added api request from  the one api

--- log-bok-kristian-jimmefors.md
+++ log-bok-kristian-jimmefors.md
@@ -1,13 +1,27 @@
 # Logbook
 
+## 2021-02-23 kl 11:30
+
+Finished the text aboute me on the aboute page
+
+Finieshed the api request so when you click a button you get 100 quotes from the lord of the rings and the hobbit movies
+
+Had trouble finding finding the api key to use for some image apis so I looked for a new api to use and found a Lord of the rings api that was easy to find the api key to
+
+Started working on the api request
+
 ## 2021-02-17 kl 14:00
 
-Started on the about me page
+Finished everything on the about page except the informatino about me
+
+Started on the about page
 
 Changed the home page
 
 ## 2021-02-16 kl 14:00
 
 Started the initial settup
+
+Finished the home page
 
 Started working on the home page
---
+++ main.js
@@ -0,0 +1,42 @@
+const containerDiv = document.getElementById("content-container");
+let characters = [];
+let movie = [["5cd95395de30eff6ebccde5c", "the fellowship of the ring"], ["5cd95395de30eff6ebccde5b", "the two towers"], ["5cd95395de30eff6ebccde5d", "return of the king"]];
+
+//gets all the characters and put them with ID and name in the characters array
+fetch("https://the-one-api.dev/v2/character", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + "sqhij0G-eoz3d5CGuBe9",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) =>
+        data.docs.forEach(element => {
+            let character = [element._id, element.name]
+            characters.push(character);
+        })
+        );
+
+function getMovieQuotes() {
+    
+  fetch("https://the-one-api.dev/v2/quote?limit=100", {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + "sqhij0G-eoz3d5CGuBe9",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) =>
+    data.docs.forEach((element) => {
+        //finds the character by ID so you can get the name of that character
+        let findCharacter = characters.find(e => e[0] == element.character);
+        let charName = findCharacter[1];
+
+        //creates a paragraph where the qoute and the name of the character that said that quote
+        let quotePara = document.createElement("p");
+        let quote = document.createTextNode('"' + element.dialog + '" -' + charName);
+        quotePara.appendChild(quote);
+        containerDiv.appendChild(quotePara);
+      })
+    );
+}
